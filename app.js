912d47c8012650259181e4427be887ea9633f1b7
infinity899 <pirvulescu.andrei91@yahoo.com>
2018-03-27 18:41:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
-var players = ['#darts'/*, '#darts2', '#darts3'*/];
-
-for (var player in players) {
-  new Vue({
-    el: players[player],
-    data: {
+Vue.component('darts_player', {
+  data: function () {
+    return {
       current_value: null,
-      score: 0
-    },
-    methods: {
-      // adunare: function(event) {
-      //   var sageata = parseInt(this.value);
-      //   return this.score+= sageata;
-      // },
-      reseter: function(event) {
-        this.current_value = 0;
-        this.score = 0;
-      },
-      submit: function(event) {
-        this.score += parseInt(this.current_value);
-        this.current_value = null;
-      }
+      score: 0,
     }
-  });
-}
+  },
+  template: '<div>' +
+  '<input type="number"' +
+  'v-model="current_value" @keypress.enter="submit">' +
+  '<p>Current value: {{ current_value }}</p>' +
+  '<p>Current round: {{ score }}</p>' +
+  '<button @click="reseter()">Reset score</button>' +
+  '</div>',
+  methods: {
+    submit: function(event) {
+      this.score += parseInt(this.current_value);
+      this.current_value = null;
+    },
+    reseter: function(event) {
+      this.current_value = 0;
+      this.score = 0;
+    }
+  }
+})
+
+new Vue({
+  el: '#components-demo',
+  data: {
+    inc: 0
+  }
+})
