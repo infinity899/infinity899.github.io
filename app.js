8faac44bb0550b5107063e847bc1b27eb9539469
infinity899 <pirvulescu.andrei91@yahoo.com>
2018-03-27 14:36:07 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-new Vue({
-        el: '#darts',
-        data: {
-            value: 0,
-            score: 0
-        },
-        methods: {
-            adunare: function(event) {
-                var sageata = parseInt(this.value);
-                return this.score+= sageata;
-            },
-            reseter: function(event) {
-                 this.sageata = 0; 
-                 this.score = 0;
-                 this.value = 0;
-                 return this;
-            }
-        }
-    });
+var players = ['#darts', '#darts2', '#darts3']
+
+for (var player in players) {
+  new Vue({
+    el: players[player],
+    data: {
+      value: 0,
+      score: 0
+    },
+    methods: {
+      adunare: function(event) {
+        var sageata = parseInt(this.value);
+        return this.score+= sageata;
+      },
+      reseter: function(event) {
+        this.sageata = 0;
+        this.score = 0;
+        this.value = 0;
+        return this;
+      }
+    }
+  });
+}
