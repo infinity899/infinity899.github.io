6f6fd17f1799e72fa110bdc76c475a7abc46c87c
infinity899 <pirvulescu.andrei91@yahoo.com>
2018-03-29 16:01:22 +0300

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,15 @@ Vue.component('darts_player', {
       score: 0,
     }
   },
-  template: '<div>' +
+  template: '<div class="player">' +
+  '<div class="player-score">' +
   '<input type="number"' +
   'v-model="current_value" @keypress.enter="submit">' +
   '<p>Current value: {{ current_value }}</p>' +
   '<p>Current round: {{ score }}</p>' +
   '<button @click="reseter()">Reset score</button>' +
+  '</div>' +
+  '<div class="progress-bar" :style="myStyle"></div>' +
   '</div>',
   methods: {
     submit: function(event) {
@@ -20,6 +23,13 @@ Vue.component('darts_player', {
     reseter: function(event) {
       this.current_value = 0;
       this.score = 0;
+    }
+  },
+  computed: {
+    myStyle: function() {
+      return {
+        height: this.score / 2 + 'px'
+      }
     }
   }
 })
